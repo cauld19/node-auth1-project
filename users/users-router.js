a0b79d9aa78f31733274fac58c88ed1ae91be2f9
cauld19 <50849710+cauld19@users.noreply.github.com>
2020-02-12 20:10:06 -0800

    get with auth working

--- users/users-router.js
+++ users/users-router.js
@@ -1,5 +1,38 @@
 const router = require('express').Router();
 
+const bcrypt = require('bcryptjs')
+
 const Users = require('../users/users-model.js')
+
+
+
+
+router.get('/', auth, (req,res) => {
+    
+    Users.find()
+        .then(users => {
+            res.status(200).json(users)
+        })
+        .catch( err => {
+            res.status(500).json(err)
+        })
+})
+
+function auth(req, res, next) {
+    const { username, password } = req.headers;
+
+    Users.findBy({username})
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                next()
+            } else {
+                res.status(401).json({message: "invalid credentials"})
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+}
 
 module.exports = router;
